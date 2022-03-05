7bb55a0a7d4061ace8be4004602873d57a91312f
ahmadsyaifulakbar24 <ipulbelcram@gmail.com>
2022-03-05 19:27:42 +0700

    repair product resposen

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Product;
 
+use App\Models\ProductCombination;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductResource extends JsonResource
@@ -14,6 +15,7 @@ class ProductResource extends JsonResource
      */
     public function toArray($request)
     {
+        $main_product = ProductCombination::where([['product_id', $this->id], ['main', 1]])->first();
         return [
             'id' => $this->id,
             'product_name' => $this->product_name,
@@ -23,6 +25,13 @@ class ProductResource extends JsonResource
             'rate' => $this->rate,
             'status' => $this->status,
             'price' => $this->price,
+            'main_product' => [
+                'id' => $main_product->id,
+                'product_slug' => $main_product->product_slug,
+                'combination_string' => $main_product->combination_string,
+                'price' => $main_product->price,
+                'unique_string' => $main_product->unique_string,
+            ],
             'image' => $this->product_image()->first()->product_image_url
         ];
     }
